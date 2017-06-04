7ab5ff3810ebd3ee92265a9495ee6601b2538451
peterticketfly <peter.vanrensburg@ticketfly.com>
2017-06-03 18:01:20 -0700

    added tail recursion for constant stack depth

--- src/main/scala/com/nefilim/nitro/Josephus.scala
+++ src/main/scala/com/nefilim/nitro/Josephus.scala
@@ -1,24 +1,44 @@
 package com.nefilim.nitro
 
+import scala.annotation.tailrec
+
 /**
   * Created by peter.vanrensburg on 5/31/17.
   */
 object Josephus extends App {
+  sealed trait Solver
+  case object RecursiveSolver extends Solver
+  case object TailRecursiveSolver extends Solver
 
-  def go(n: Int, k: Int): Int = {
-    if (n < 1 || k < 1)
-      0
-    else if (n == 1)
-      1
-    else {
-      ((go(n - 1, k) + k - 1) % n) + 1
+  def solve(n: Int, k: Int, solver: Solver = TailRecursiveSolver): Int = {
+    @tailrec
+    def go_tail(i: Int, k: Int, accumulator: Int): Int = {
+      i match {
+        case 0 => accumulator
+        case _ => go_tail(i - 1, k, ((accumulator + k - 1) % (n - i + 1)) + 1)
+      }
+    }
+
+    def go(i: Int, k: Int): Int = {
+      i match {
+        case 1 => 1
+        case _ => ((go(i - 1, k) + k - 1) % i) + 1
+      }
+    }
+
+    if (n < 0 || k < 1)
+      return 0
+
+    solver match {
+      case TailRecursiveSolver => go_tail(n, k, 1)
+      case RecursiveSolver => go(n, k)
     }
   }
 
   if (args.length > 1) {
     val n = args(0).toInt
     val k = args(1).toInt
-    println(go(n, k))
+    solve(n, k)
   } else
     println("Usage: sbt run n k")
 }
--- src/test/scala/com/nefilim/nitro/JosephusSpec.scala
+++ src/test/scala/com/nefilim/nitro/JosephusSpec.scala
@@ -10,27 +10,31 @@ class JosephusSpec extends FlatSpec with Matchers {
 
   "k=2" should "match known solution" in {
     val solution = List[Int](1, 1, 3, 1, 3, 5, 7, 1, 3, 5, 7, 9, 11, 13, 15, 1)
-    (1 to 16).map(go(_, 2)) should be (solution)
+    (1 to 16).map(solve(_, 2, TailRecursiveSolver)) should be (solution)
+    (1 to 16).map(solve(_, 2, RecursiveSolver)) should be (solution)
   }
 
   "Invalid parameters" should "return 0" in {
-    go(-1, 1) should be (0)
-    go(1, 0) should be (0)
-    go(5, -2) should be (0)
+    solve(-1, 1) should be (0)
+    solve(1, 0) should be (0)
+    solve(5, -2) should be (0)
   }
 
   "f(k, k) " should "never be k" in {
     (2 to 1024).foreach { i =>
-      go(i, i) should not be (i)
+      solve(i, i) should not be (i)
     }
   }
 
   "n = 3 and k = 2" should "work" in {
-    go(3, 2) should be (3)
+    solve(3, 2) should be (3)
   }
 
-  "n is very large but k is 2" should "throw a StackOverflowException" in {
-    an [StackOverflowError] should be thrownBy go(1024 * 1024, 2)
+  "n is very large but k is 2 for non tail recursive" should "throw a StackOverflowException" in {
+    an [StackOverflowError] should be thrownBy solve(1024 * 1024, 2, RecursiveSolver)
   }
 
+  "n is very large but k is 2 for tail recursive" should "NOT throw a StackOverflowException" in {
+    noException should be thrownBy solve(1024 * 1024, 2, TailRecursiveSolver)
+  }
 }
